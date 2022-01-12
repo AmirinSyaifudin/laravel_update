62709c778cab4efb92008e726d271e0cdf82369f
AmirinSyaifudin <amirinsyaifudin6@gmail.com>
2022-01-12 06:45:11 -0800

    Tampila data kota

--- app/Http/Controllers/Admin/KotaController.php
+++ app/Http/Controllers/Admin/KotaController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Provinsi;
+use App\Kota;
+
 
 class KotaController extends Controller
 {
@@ -26,6 +30,28 @@ class KotaController extends Controller
     public function create()
     {
         //
+        $kota = DB::table('kota')
+        ->join('provinsi','kota.provinsi_id','=','provinsi_id')
+        ->select([
+            'provinsi.provinsi_id',
+            'provinsi.nama_provinsi',
+            'kota.nama_kota',
+            'kota.kode_pos',
+            'kota.keterangan',
+        ])
+        ->get();
+
+        $provinsi = DB::table('provinsi')->get();
+
+        $data = array(
+            'kota'     => $kota,
+            'provinsi'  =>$provinsi,
+        );
+
+        // dd($data);
+
+        return redirect('admin/kota', $data)
+        ->with('sukses','Data kota Berhasil di tambahkan !!! ');
     }
 
     /**
@@ -37,6 +63,25 @@ class KotaController extends Controller
     public function store(Request $request)
     {
         //
+       DB::table('kota')
+        ->insert([
+            'nama_kota'         => $request->nama_kota,
+            'kode_pos'          => $request->kode_pos,
+            'keterangan'        => $request->keterangan,
+            'nama_provinsi'     => $request->nama_provinsi,
+        ]);
+
+        // $provinsi = DB::table('provinsi')->get();
+
+        // $data = array(
+        //     'kota'     => $kota,
+        //     'provinsi'  =>$provinsi,
+        // );s
+
+        // dd($data);
+
+        return redirect('admin/kota')
+        ->with('sukses','Data kota Berhasil di tambahkan !!! ');
     }
 
     /**
--- resources/views/admin/kota/index.blade.php
+++ resources/views/admin/kota/index.blade.php
@@ -6,7 +6,11 @@
  <div class="box">
         <div class="box-header">
               <h3 class="box-title">DATA KOTA</h3><br><br>
-              <a href="" class="btn btn-primary">ADD KOTA</a>
+              <button type="button" class="btn btn-primary" data-toggle="modal" 
+                      data-target="#exampleModal"><a>ADD DATA KOTA</a>
+              </button>
+              <a href="" class="btn btn-primary">Export Excel</a> 
+              <a href="" class="btn btn-primary">Export PDF</a>  
          </div>
          <div class="box-body">
 
@@ -30,6 +34,68 @@
             </table>
         </div>
 </div>
+
+
+ <!-- Modal -->
+ <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+         <div class="modal-content">
+              <div class="modal-header">
+                   <h5 class="modal-title" id="exampleModalLabel">ADD DATA KOTA</h5>
+                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                   <span aria-hidden="true">&times;</span>
+                   </button>
+               </div>
+         
+
+           <div class="modal-body">                    
+    
+               <form action="{{ route('admin.kota.store') }}" method="POST" enctype="multipart/form-data"> 
+                       @csrf
+
+                       <div class="form-group>
+                            <label for="">NAMA PROVINSI</label>
+                            <select name="provinsi_id" id="validationCustom03" class="form-control select2">
+                              
+                                @error('provinsi')
+                                    <span class="help-block">{{ $message}}</span>
+                                @enderror
+                            </select>
+                        </div>
+
+                       <div class="form-group @error('nama_kota') has-error @enderror">
+                            <label for="">NAMA KOTA</label>
+                            <input type="text" name="nama_kota" class="form-control" placeholder=""
+                            value="{{ old('nama_kota') }}">
+                            @error('nama_kota')
+                                <span class="help-block">{{ $message}}</span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group @error('kode_pos') has-error @enderror">
+                            <label for="">KODE POS</label>
+                            <input type="number" name="kode_pos" class="form-control" placeholder=""
+                            value="{{ old('kode_pos') }}">
+                            @error('kode_pos')
+                                <span class="help-block">{{ $message}}</span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">KETERANGAN</label>
+                            <textarea name="keterangan" class="form-control" id="exampleFormControlTextarea1" rows="3">{{old('keterangan')}}</textarea>
+                        </div>  
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">KEMBALI</button>
+                        <button type="submit" class="btn btn-primary">SIMPAN DATA</button>
+                    </div>
+                </form>
+                
+           </div>
+       </div>
+   </div>
+ </div>
 
 
 @endsection
